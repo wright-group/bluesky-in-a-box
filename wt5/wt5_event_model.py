b79cdd4bdfc411d05a32fd362d01e36539f416b5
Kyle Sunden <git@ksunden.space>
2021-09-23 14:51:41 -0500

    Allow for more streams than just primary
    Untested because of instability of my machine, pushing to keep the changes as I reinstall my OS...

--- wt5/wt5_event_model.py
+++ wt5/wt5_event_model.py
@@ -13,13 +13,12 @@ class GenWT5(CallbackBase):
     def __init__(self):
         self.start_doc = None
         self.stop_doc = None
-        self.descriptor_doc = None
-        self.data = None
-        self.shape = None
-        self.scan_shape = None
+        self.descriptor_docs = {}
+        self.data = {}
+        self.shape = {}
+        self.scan_shape = {}
         self.run_dir = None
         self.bluesky_doc_dir = None
-        self.stream_name = None
         self.dims = {}
 
     def start(self, doc):
@@ -39,23 +38,23 @@ class GenWT5(CallbackBase):
         with open(self.bluesky_doc_dir / "start.json", "wt") as f:
             json.dump(self.start_doc, f, indent=2)
 
-        self.shape = list(
+        self.shape["primary"] = list(
             self.start_doc.get("shape", [self.start_doc.get("num_points")])
         )
-        self.scan_shape = tuple(self.shape)
+        self.scan_shape["primary"] = tuple(self.shape)
 
     def descriptor(self, doc):
-        self.descriptor_doc = doc
-        self.stream_name = self.descriptor_doc["name"]
+        stream_name = doc["name"]
+        self.descriptor_docs[stream_name] = doc
 
         with open(
-            self.bluesky_doc_dir / f"{self.stream_name} descriptor.json", "wt"
+            self.bluesky_doc_dir / f"{stream_name} descriptor.json", "wt"
         ) as f:
-            json.dump(self.descriptor_doc, f, indent=2)
+            json.dump(doc, f, indent=2)
 
-        self.data = wt.Data(
-            self.run_dir / f"{self.stream_name}.wt5",
-            name=self.stream_name,
+        self.data[stream_name] = wt.Data(
+            self.run_dir / f"{stream_name}.wt5",
+            name=stream_name,
             edit_local=True,
         )
 
@@ -66,59 +65,59 @@ class GenWT5(CallbackBase):
                 print(
                     f"Skipping key {repr(k)} from start document in metadata because it cannot be placed in HDF5 attrs"
                 )
-        self.data.attrs["created"] = wt.kit.TimeStamp(self.start_doc["time"]).RFC3339
+        self.data[stream_name].attrs["created"] = wt.kit.TimeStamp(self.start_doc["time"]).RFC3339
 
         # compute full shape, channel shapes
         # handling of dims may need adjustment for cameras...
         chan_shapes = {}
-        self.dims = {}
-        for k, desc in self.descriptor_doc["data_keys"].items():
+        self.dims[stream_name] = {}
+        for k, desc in doc["data_keys"].items():
             chan_shape = desc.get("shape", [])
             chan_dims = desc.get("dims", [])
-            if any(dim in self.dims for dim in chan_dims):
-                chan_shapes[k] = self.dims[chan_dims[0]]
+            if any(dim in self.dims[stream_name] for dim in chan_dims):
+                chan_shapes[k] = self.dims[stream_name][chan_dims[0]]
             else:
                 chan_shapes[k] = (
-                    list(self.scan_shape)
-                    + [1] * (len(self.shape) - len(self.scan_shape))
+                    list(self.scan_shape[stream_name])
+                    + [1] * (len(self.shape[stream_name]) - len(self.scan_shape[stream_name]))
                     + list(chan_shape)
                 )
-                self.shape += desc.get("shape", [])
+                self.shape[stream_name] += desc.get("shape", [])
                 for dim in chan_dims:
-                    self.dims[dim] = chan_shapes[k]
+                    self.dims[stream_name][dim] = chan_shapes[k]
 
-        self.data.create_variable(
+        self.data[stream_name].create_variable(
             "labtime",
-            shape=self.scan_shape + (1,) * (len(self.shape) - len(self.scan_shape)),
+            shape=self.scan_shape[stream_name] + (1,) * (len(self.shape[stream_name]) - len(self.scan_shape[stream_name])),
             units="s",
         )
         for k, chan_shape in chan_shapes.items():
-            chan_shape += [1] * (len(self.shape) - len(chan_shape))
-            units = self.descriptor_doc["data_keys"][k].get("units")
+            chan_shape += [1] * (len(self.shape[stream_name]) - len(chan_shape))
+            units = self.descriptor_doc[stream_name]["data_keys"][k].get("units")
             if (
                 any(
-                    k in self.descriptor_doc["object_keys"][det]
+                    k in self.descriptor_doc[stream_name]["object_keys"][det]
                     for det in self.start_doc.get("detectors")
                 )
-                and not k in self.dims
+                and not k in self.dims[stream_name]
             ):
-                self.data.create_channel(k, shape=chan_shape, units=units)
+                self.data[stream_name].create_channel(k, shape=chan_shape, units=units)
             else:
-                self.data.create_variable(k, shape=chan_shape, units=units)
-            for vk, v in self.descriptor_doc["data_keys"][k].items():
+                self.data[stream_name].create_variable(k, shape=chan_shape, units=units)
+            for vk, v in self.descriptor_doc[stream_name]["data_keys"][k].items():
                 if vk in ("shape", "units", "dtype"):
                     continue
-                self.data[k].attrs[vk] = v
+                self.data[stream_name][k].attrs[vk] = v
 
         if "plan_pattern" in self.start_doc:
             if self.start_doc["plan_pattern"] == "outer_product":
-                add_outer_product_axes(self.data, self.start_doc["plan_pattern_args"]["args"], self.start_doc["motors"], self.start_doc["plan_axis_units"])
+                add_outer_product_axes(self.data[stream_name], self.start_doc["plan_pattern_args"]["args"], self.start_doc["motors"], self.start_doc["plan_axis_units"])
             elif self.start_doc["plan_pattern"] == "outer_list_product":
-                add_outer_list_product_axes(self.data, self.start_doc["plan_pattern_args"]["args"], self.start_doc["motors"], self.start_doc["plan_axis_units"])
+                add_outer_list_product_axes(self.data[stream_name], self.start_doc["plan_pattern_args"]["args"], self.start_doc["motors"], self.start_doc["plan_axis_units"])
             elif self.start_doc["plan_pattern"] == "inner_product":
-                add_inner_product_axes(self.data, self.start_doc["plan_pattern_args"]["args"], self.start_doc["plan_pattern_args"]["num"], self.start_doc["motors"], self.start_doc["plan_axis_units"])
+                add_inner_product_axes(self.data[stream_name], self.start_doc["plan_pattern_args"]["args"], self.start_doc["plan_pattern_args"]["num"], self.start_doc["motors"], self.start_doc["plan_axis_units"])
             elif self.start_doc["plan_pattern"] == "inner_list_product":
-                add_inner_list_product_axes(self.data, self.start_doc["plan_pattern_args"]["args"], self.start_doc["motors"], self.start_doc["plan_axis_units"])
+                add_inner_list_product_axes(self.data[stream_name], self.start_doc["plan_pattern_args"]["args"], self.start_doc["motors"], self.start_doc["plan_axis_units"])
 
         for hw, (units, terms) in self.start_doc.get("plan_constants", {}).items():
             terms.append([-1, hw])
@@ -126,24 +125,25 @@ class GenWT5(CallbackBase):
             terms = list(filter(lambda x: x[1] is not None, terms))
             if const_term < 0:
                 terms = [[-1 * coeff, var] for coeff, var in terms]
-            c = self.data.create_constant(
+            c = self.data[stream_name].create_constant(
                 "+".join(f"{coeff}*{var}_readback" for coeff, var in terms)
             )
             c.units = units
 
-        self.data.flush()
+        self.data[stream_name].flush()
 
         # what to do about md that is nested... attrs doesn't like that
         # What to do about lower dimensional axes... not trivial... could parse out from plan pattern, I guess... I think that is the intended way...
 
     def event(self, doc):
-        pos = np.unravel_index(doc["seq_num"] - 1, self.scan_shape)
-        self.data["labtime"][pos + (...,)] = doc["time"]
+        stream_name = "primary" # TODO get stream name properly
+        pos = np.unravel_index(doc["seq_num"] - 1, self.scan_shape[stream_name])
+        self.data[stream_name]["labtime"][pos + (...,)] = doc["time"]
         for var, entry in doc["data"].items():
-            pos_var = [slice(None) if j > 1 else 0 for j in self.data[var].shape]
-            pos_var[: len(self.scan_shape)] = pos
-            self.data[var][tuple(pos_var)] = entry
-        self.data.flush()
+            pos_var = [slice(None) if j > 1 else 0 for j in self.data[stream_name][var].shape]
+            pos_var[: len(self.scan_shape[stream_name])] = pos
+            self.data[stream_name][var][tuple(pos_var)] = entry
+        self.data[stream_name].flush()
 
     def stop(self, doc):
         try:
@@ -157,47 +157,49 @@ class GenWT5(CallbackBase):
 
             # transform (axes make filling harder than it needs to be)
             try:
-                self.data.transform(
+                self.data["primary"].transform(
                     *(
                         f"{x}_readback"
-                        for x in self.start_doc["motors"][: len(self.scan_shape)]
+                        for x in self.start_doc["motors"][: len(self.scan_shape["primary"])]
                     ),
-                    *self.dims,
+                    *self.dims["primary"],
                 )
             except KeyError:
-                self.data.transform("labtime", *self.dims)
+                self.data["primary"].transform("labtime", *self.dims["primary"])
 
-            self.data.flush()
+            self.data["primary"].flush()
 
-            with open(
-                self.run_dir / f"{self.descriptor_doc['name']} tree.txt", "wt"
-            ) as f:
-                with contextlib.redirect_stdout(f):
-                    self.data.print_tree(verbose=True)
+            for name, descriptor_doc in self.descriptor_docs.items():
+                with open(
+                    self.run_dir / f"{name} tree.txt", "wt"
+                ) as f:
+                    with contextlib.redirect_stdout(f):
+                        self.data["name"].print_tree(verbose=True)
 
-            for dev, hints in self.descriptor_doc["hints"].items():
-                for chan in hints["fields"]:
-                    if not chan in self.data.channel_names:
-                        continue
-                    try:
-                        wt.artists.quick2D(
-                            self.data,
-                            channel=chan,
-                            autosave=True,
-                            save_directory=self.run_dir,
-                            fname=chan,
-                        )
-                    except (wt.exceptions.DimensionalityError, IndexError):
-                        wt.artists.quick1D(
-                            self.data,
-                            channel=chan,
-                            autosave=True,
-                            save_directory=self.run_dir,
-                            fname=chan,
-                        )
-        finally:
-            self.data.flush()
-            self.data.close()
+                for dev, hints in descriptor_doc["hints"].items():
+                    for chan in hints["fields"]:
+                        if not chan in self.data[name].channel_names:
+                            continue
+                        try:
+                            wt.artists.quick2D(
+                                self.data[name],
+                                channel=chan,
+                                autosave=True,
+                                save_directory=self.run_dir,
+                                fname=chan,
+                            )
+                        except (wt.exceptions.DimensionalityError, IndexError):
+                            wt.artists.quick1D(
+                                self.data[name],
+                                channel=chan,
+                                autosave=True,
+                                save_directory=self.run_dir,
+                                fname=chan,
+                            )
+            finally:
+                for d in self.data.values():
+                    d.flush()
+                    d.close()
 
 
 def add_outer_product_axes(data, pattern_args, motors, axis_units):
