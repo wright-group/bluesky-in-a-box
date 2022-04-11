65a1ea0c0657a93d7355524b2ede3e984f89c92f
Kyle Sunden <git@ksunden.space>
2022-04-11 17:16:33 -0500

    Deal with sub devices
    Eject subdevices from the top level namespace, make them accessible via dot notatation to the queueserver

--- re-manager/startup.py
+++ re-manager/startup.py
@@ -12,8 +12,7 @@ from wright_plans import (
     rel_scan_wp,
 )
 from wright_plans.attune import (
-    motortune,
-    run_tune_test,
+    motortune, run_tune_test,
     run_intensity,
     run_setpoint,
     run_holistic,
@@ -25,7 +24,11 @@ from bluesky.protocols import Movable
 
 happi_client = happi.Client(database=happi.backends.backend("/happi_db.json"))
 
+# Movable devices to read baseline before and after scans
 movables = []
+
+# Cache of describe keys used to determine if sub-devices should be ejected from the namespace
+all_device_keys = {}
 
 # Host mapped name on windows and mac
 host = "host.internal.docker" 
@@ -35,15 +38,41 @@ except socket.gaierror:
     host = "172.17.0.1"  # Default host ip on Linux
 
 for device in happi_client.all_items:
+    # Skip devices marked inactive in happi
+    if not device.active:
+        continue
     try:
+        # Translate localhost into internal host machine reference
         if device.host in ("localhost", "127.0.0.1"):
             device.host = host
-        vars()[device.name] = happi.from_container(device)
-        if isinstance(vars()[device.name], Movable):
-            movables.append(vars()[device.name])
+        dev = happi.from_container(device)
+        dev_keys = set(dev.describe().keys())
+        for prev_dev_name, prev_dev_keys in all_device_keys.items():
+            # Do not add this device (break for loop, skip else clause)
+            # if all of my keys are in another device
+            if dev_keys.issubset(prev_dev_keys):
+                break
+            # Eject (set to None) a subdevice previously added
+            if prev_dev_keys.issubset(dev_keys):
+                vars()[prev_dev_name] = None
+                del all_device_keys[prev_dev_name]
+        else:
+            # Only runs if this is a new device (no break above)
+            print("Adding", device.name)
+            # Add to namespace
+            vars()[device.name] = dev
+            # Add keys to cache
+            all_device_keys[dev.name] = dev_keys
+            # Add to movables list if Movable
+            if isinstance(dev, Movable):
+                movables.append(dev)
     except Exception as e:
         print(e)
 
+dev = None
+prev_dev = None
+
+# Wrap all of the plans with baseline which reads movables before and after
 list_scan_wp = baseline_decorator(movables)(list_scan_wp)
 rel_list_scan_wp = baseline_decorator(movables)(rel_list_scan_wp)
 list_grid_scan_wp = baseline_decorator(movables)(list_grid_scan_wp)
