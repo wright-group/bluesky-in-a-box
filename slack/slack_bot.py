248fec2fa842e25a10fbf9682dd1e513baf293fd
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2024-02-29 16:51:15 -0600

    revisions, implement plot command (#55)
    * ready for testing
    
    * testing works
    
    * plot method ready for tests
    
    * Update slack_bot.py
    
    * plot working
    
    ---------
    
    Co-authored-by: wright-stuff <wright_stuff_chem@wisc.edu>

--- slack/slack_bot.py
+++ slack/slack_bot.py
@@ -5,100 +5,105 @@ import pathlib
 
 from slack_bolt.app.async_app import AsyncApp
 from slack_bolt.adapter.socket_mode.async_handler import AsyncSocketModeHandler
-from slack_sdk.errors import SlackApiError
-
-from slack_event_model import Acquisition
 
 from bluesky.callbacks.zmq import RemoteDispatcher
 
+from slack_event_model import Acquisition
+from lib import async_client_method_handler as client_handler
+
 
 logging.basicConfig(level=logging.DEBUG)
-logging.info(os.environ["SLACK_BOT_TOKEN"])
-logging.info(os.environ["SLACK_APP_TOKEN"])
 
-
-class SlackApp(AsyncApp):
-    def post_message(self, **kwargs):
-        try:
-            logging.info(f"posting message to {kwargs['channel']}")
-            asyncio.create_task(self.client.chat_postMessage(**kwargs))
-
-        except Exception as e:  # SlackApiError as e:
-            self.logger.error(f"Error posting message: {e}")
-
-    def files_upload(self, **kwargs):
-        try:
-            logging.info(f"uploading file to {kwargs['channel']}")
-            asyncio.create_task(self.client.files_upload_v2(**kwargs))
-
-        except Exception as e:  # SlackApiError as e:
-            self.logger.error(f"Error posting message: {e}")
-
-
-app = SlackApp(token=os.environ["SLACK_BOT_TOKEN"])
-
+app = AsyncApp(token=os.environ["SLACK_BOT_TOKEN"])
 desired_message = re.compile(
-    r"(?P<name>\w+) (?P<command>(fetch|plot)) (?P<id>\w+)"
+    r"(?P<user><@\w+>)\s+(?P<command>(fetch|plot))\s+(?P<args>\w+)"
 )
 
-# bind app to event callbacks
-@app.message(re.compile(desired_message))
-async def parse_message(message, say):
-    app.logger.info(f"we got a message: {message}")
-    # message: https://api.slack.com/events/message
-    match = re.match(desired_message, message["text"])
-    if match is None:
-        await say(":thinking_face: I didn't understand this request")
-        return
-    else:
-        command = match["command"]
-        await say(f"I understand you want me to {command} with id {match['id']}")
-        if command == "fetch":
-            fetch_by_id(app, match['id'], message)
-        elif command == "plot":
-            plot_by_id(app, match['id'], message)
-
-@app.command("/hello-socket-mode")
-async def hello_command(ack, body):
-    user_id = body["user_id"]
-    await ack(f"Hi <@{user_id}>!")
 
 @app.event("app_mention")
-async def event_test(event, say):
-    await say(f"Hi there, <@{event['user']}>!" + f"event keys are {event.keys()}")
+async def parse_user_request(event, say):
+    match = re.match(desired_message, event["text"])
+    if match is None:
+        await say(f":thinking_face: I didn't understand request `{event['text']}`")
+        return
+    command = match["command"]
+    if command == "fetch":
+        status = fetch(app, match['args'], event)
+        if status != 1:
+            await say(
+                f"I need a single match, but I found {status} acquisition(s) matching your" \
+                + f"specifier `{match['args']}` :weary:"
+            )
+    elif command == "plot":
+        specifier, *channels = match["args"].split()
+        status = plot(app, specifier, event, channels if channels else [""])
+        logging.debug("status {status}")
+
+
+def plot(app, specifier, meta, channels):
+    paths = _find_acquisition_file(specifier)
+    status = len(paths)
+    if status == 1:
+        file_uploads = []
+        for channel in channels:
+            for cpath in paths[0].glob(f"{channel}*.png"):
+                file_uploads.append(dict(file=str(cpath), title=cpath.name))
+        logging.info(f"paths {paths} | channels {channels} | file_uploads {file_uploads}")
+        # truncate files to 10
+        note = ""
+        if len(file_uploads) > 10:
+            file_uploads = file_uploads[:10]
+            note = "We truncated the images uploaded to 10."
+        status = len(file_uploads) > 0
+        scan_folder = paths[0].parts[-2]
+        if status:
+            client_handler(
+                app.client.files_upload_v2,
+                initial_comment=f"<@{meta['user']}> images from `{scan_folder}`. {note}",
+                channel=meta["channel"],
+                file_uploads=file_uploads,
+                thread_ts=meta.get("thread_ts", meta["ts"]),
+            )
+    return status
+
+
+def _find_acquisition_file(specifier):
+    return list(pathlib.Path("/data").glob(f"*{specifier}*"))
+
+
+def fetch(app:AsyncApp, specifier, meta):
+    paths = _find_acquisition_file(specifier)
+    status = len(paths)
+    if status == 1:
+        path = paths[0] / "primary.wt5"
+        scan_name = path.parts[-2]
+        # TODO: check existence, check file size
+        client_handler(
+            app.client.files_upload_v2,
+            initial_comment=f"<@{meta['user']}> fetched from `{scan_name}`",
+            channel=meta["channel"],
+            filename= f"{scan_name}_primary.wt5",
+            thread_ts=meta.get("thread_ts", meta["ts"]),
+            file=str(path),
+        )
+    return status
+
 
 @app.event("message")
-async def handle_message_events(body, logger):
-    logger.debug(body)
+async def handle_message(body, logger):
+    logger.info(body)
 
-def plot_by_id(app, id, message):
-    print("plotting is not implemented yet")
-    ...
-
-def fetch_by_id(app:SlackApp, id, message):
-    id_exists = [_ for _ in pathlib.Path("/data").glob(f"* {id}")]
-    if id_exists:
-        file = id_exists[0] / "primary.wt5"
-        logging.debug(f"channel {message['channel']} file {file} exists? {file.exists()}")
-        app.files_upload(
-            initial_comment=f"<@{message['user']}> here is {id}!",
-            channel=message["channel"],
-            filename="primary.wt5",
-            file=str(file),
-        )
 
 async def main():
     handler = AsyncSocketModeHandler(app, app_token=os.environ["SLACK_APP_TOKEN"])
     await handler.connect_async()
 
     loop = asyncio.get_running_loop()
-    logging.info(f"loop:{loop}")
     dispatcher = RemoteDispatcher("zmq-proxy:5568", loop=loop)
     dispatcher.subscribe(Acquisition(app, os.environ.get("SLACK_CHANNEL")))
-    logging.info("adding poll to loop")
     await dispatcher._poll()
+
     await handler.disconnect_async()
-    logging.info("closing")
 
 
 if __name__ == "__main__":
