37c7af7009ca483fefc094e15365c859d5c9082f
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2026-04-18 15:46:58 -0500

    try to fix numpy issue

--- re-manager/start_re.sh
+++ re-manager/start_re.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-# qserver-list-plans-devices --startup-script startup.py
+qserver-list-plans-devices --startup-script startup.py
 start-re-manager --startup-script=./startup.py --existing-plans-devices=./existing_plans_and_devices.yaml --user-group-permissions=./user_group_permissions.yaml --redis-addr redis:6379 --zmq-publish-console ON
