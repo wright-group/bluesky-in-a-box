f6efc4ac3abb380cf03c4e242f36c7074921c022
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2026-04-22 22:23:27 -0500

    Update startup.py

--- tests/numpy-test/startup.py
+++ tests/numpy-test/startup.py
@@ -1,5 +1,2 @@
 from bluesky.plans import count
-from bluesky.preprocessors import baseline_decorator
-
-# Wrap all of the plans with baseline which reads movables before and after
-# count = baseline_decorator(movables)(count)
+# from bluesky.preprocessors import baseline_decorator
