3f5088e87b2cd9b9637252e850546f69e53abf0a
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2026-04-22 22:21:28 -0500

    Update startup.py
    only import statements

--- tests/numpy-test/startup.py
+++ tests/numpy-test/startup.py
@@ -1,14 +1,5 @@
 from bluesky.plans import count
 from bluesky.preprocessors import baseline_decorator
 
-# Movable devices to read baseline before and after scans
-movables = []
-
-# Cache of describe keys used to determine if sub-devices should be ejected from the namespace
-all_device_keys = {}
-
-dev = None
-prev_dev = None
-
 # Wrap all of the plans with baseline which reads movables before and after
-count = baseline_decorator(movables)(count)
+# count = baseline_decorator(movables)(count)
