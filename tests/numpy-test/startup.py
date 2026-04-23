e73554aef974f52657e5f5ba58ae56320d689c7a
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2026-04-22 22:19:09 -0500

    remove wright_plans

--- tests/numpy-test/startup.py
+++ tests/numpy-test/startup.py
@@ -1,20 +1,3 @@
-from wright_plans import (
-    list_scan_wp,
-    rel_list_scan_wp,
-    list_grid_scan_wp,
-    rel_list_grid_scan_wp,
-    scan_wp,
-    grid_scan_wp,
-    rel_grid_scan_wp,
-    rel_scan_wp,
-)
-from wright_plans.attune import (
-    motortune, run_tune_test,
-    run_intensity,
-    run_setpoint,
-    run_holistic,
-)
-
 from bluesky.plans import count
 from bluesky.preprocessors import baseline_decorator
 
@@ -28,17 +11,4 @@ dev = None
 prev_dev = None
 
 # Wrap all of the plans with baseline which reads movables before and after
-list_scan_wp = baseline_decorator(movables)(list_scan_wp)
-rel_list_scan_wp = baseline_decorator(movables)(rel_list_scan_wp)
-list_grid_scan_wp = baseline_decorator(movables)(list_grid_scan_wp)
-rel_list_grid_scan_wp = baseline_decorator(movables)(rel_list_grid_scan_wp)
-scan_wp = baseline_decorator(movables)(scan_wp)
-grid_scan_wp = baseline_decorator(movables)(grid_scan_wp)
-rel_grid_scan_wp = baseline_decorator(movables)(rel_grid_scan_wp)
-rel_scan_wp = baseline_decorator(movables)(rel_scan_wp)
-motortune = baseline_decorator(movables)(motortune)
-run_tune_test = baseline_decorator(movables)(run_tune_test)
-run_intensity = baseline_decorator(movables)(run_intensity)
-run_setpoint = baseline_decorator(movables)(run_setpoint)
-run_holistic = baseline_decorator(movables)(run_holistic)
 count = baseline_decorator(movables)(count)
