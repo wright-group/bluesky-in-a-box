dd4b373a637108eeccb0446f14b5cd6f1e8164e9
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2026-04-24 16:02:50 -0500

    Update slack_bot.py

--- slack/slack_bot.py
+++ slack/slack_bot.py
@@ -103,6 +103,20 @@ def _find_acquisition_file(specifier):
     return list(pathlib.Path("/data").glob(f"*{specifier}*"))
 
 
+class NoBlockRD(RemoteDispatcher):
+    """
+    hack to interleave zmq dispatcher with slack
+    RemoteDispatcher only has a blocking utility atm
+    """
+    async def astart(self):
+        self._RemoteDispatcher__factory()
+        try:
+            await self._poll()
+        finally:
+            self.stop()
+
+
+
 async def main():
     handler = AsyncSocketModeHandler(app, app_token=os.environ["SLACK_APP_TOKEN"])
     await handler.connect_async()
@@ -110,10 +124,10 @@ async def main():
     user = await app.client.auth_test()
     logging.info(user)
 
-    dispatcher = RemoteDispatcher("zmq-proxy:5568")
+    dispatcher = NoBlockRD("zmq-proxy:5568")
     dispatcher.subscribe(Acquisition(app, os.environ.get("SLACK_CHANNEL")))
-    await dispatcher._poll()
 
+    await dispatcher.astart()
     await handler.disconnect_async()
 
 
