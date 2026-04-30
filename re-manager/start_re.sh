af5a14a9427dbec7caae1378f3ec18a8245e622c
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2026-04-30 13:31:10 -0500

    update bluesky (#79)
    * try updated startup
    
    * Update test-docker-compose.yml
    
    * Update requirements.txt
    
    * Update dependabot.yml
    
    * Update test-docker-compose.yml
    
    * hwproxy versions
    
    * re: use latest numpy
    
    not sure if we should remove the qserver-list-plans-devices, but trying it out for now...
    
    * try to fix numpy issue
    
    * Update test-docker-compose.yml
    
    * Update test-docker-compose.yml
    
    * check workspace var
    
    * don't hardcode absolute paths
    
    * some cleanup
    
    args -> kwargs for zmq startup
    re-manager: dockerfile must run qserver list-plans-devices without failure to pass tests
    numpy is still an issue
    qserer must be pinned to 0.0.22
    
    * pin bluesky-queueserver
    
    * Update .gitignore
    
    * unify bluesky, numpy versions
    
    * Update .gitignore
    
    * Update .gitignore
    
    * Update slack_bot.py
    
    runs on fs machine
    
    * Update slack_bot.py
    
    * test yaqc-bluesky-fix (#81)
    
    * test yaqc-bluesky-fix
    
    * validate test failure
    
    * reimplement fix
    
    * correct install order
    
    * Update dockerfile
    
    * Apply suggestion from @ddkohler
    
    ---------
    
    Co-authored-by: fs lab machine <wright_stuff_chem@wisc.edu>
    
    * cleanup (#82)
    
    * test yaqc-bluesky-fix
    
    * validate test failure
    
    * reimplement fix
    
    * correct install order
    
    * Update dockerfile
    
    * Apply suggestion from @ddkohler
    
    * Update dependabot.yml
    
    * Update dependabot.yml
    
    * Update slack_bot.py
    
    ---------
    
    Co-authored-by: fs lab machine <wright_stuff_chem@wisc.edu>
    
    * Update slack_bot.py
    
    just open a thread for the dispatcher; it seems like the intended way
    
    * Update slack_bot.py
    
    * resave
    
    * Update slack_bot.py
    
    ---------
    
    Co-authored-by: fs lab machine <wright_stuff_chem@wisc.edu>

--- re-manager/start_re.sh
+++ re-manager/start_re.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 qserver-list-plans-devices --startup-script startup.py
-start-re-manager --startup-script=./startup.py --existing-plans-devices=./existing_plans_and_devices.yaml --user-group-permissions=./user_group_permissions.yaml --zmq-data-proxy-addr zmq-proxy:5567 --redis-addr redis:6379 --zmq-publish-console ON
+start-re-manager --startup-script=./startup.py --existing-plans-devices=./existing_plans_and_devices.yaml --user-group-permissions=./user_group_permissions.yaml --redis-addr redis:6379 --zmq-publish-console ON
